6680c1c047bbdd21499a22bb0bf3ce4057f14098
RahulBhosale03 <143332339+RahulBhosale03@users.noreply.github.com>
2025-11-27 12:56:47 +0530

    Update output message for sorted array check

--- src/arrays/CheckArrayIsSorted.java
+++ src/arrays/CheckArrayIsSorted.java
@@ -13,6 +13,6 @@ public class CheckArrayIsSorted {
     public static void main(String[] args) {
         int arr[] = {2,4,1,5,3,7,9,8,11,10};
         int n = arr.length;
-        System.out.println(isSorted(arr,n));
+        System.out.println("The Sorted array is : "+isSorted(arr,n));
     }
 }
